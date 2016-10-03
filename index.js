17791f8cf5a2f339095756e8e9af90f9ed8c3988
Buz Carter <bcarter@fandango.com>
2016-10-03 15:02:38 -0700

    Specify supported codes.

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 var express = require('express');
 
 const SERVER_PORT = 3080;
+// will hang response 100, 101,
+const VALID_CODES = [ 200, 201, 202, 203, 204, 205, 206, 300, 301, 302, 303, 304, 305, 306, 307, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 500, 501, 502, 503, 504, 505];
 
 function apiHandler(req, res) {
   console.log('Request => (Response Code: ' + req.params.code + ', Delay: ' + req.params.delay + ' seconds)');
-  var code = parseInt(req.params.code, 10);
 
-  if (code < 100 || code > 599) {
+  var code = parseInt(req.params.code, 10);
+  if (VALID_CODES.indexOf(code) < 0) {
     return res.send('Invalid Code: ' + code);
   }
 
@@ -21,7 +23,7 @@ function apiHandler(req, res) {
   }
 
   setTimeout(function () {
-    return res.sendStatus(code);
+    res.sendStatus(code);
   }, delay * 1000);
 }
 

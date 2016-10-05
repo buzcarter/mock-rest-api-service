ff0e9f5c3ff7b2b6cf674bd5330a58453b6f5e63
Buz Carter <bcarter@fandango.com>
2016-10-05 07:29:20 -0700

    Add "random" endpoint

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A mock REST API endpoint that'll return whichever status code you request after an `n` second delay, specified by you.
 
-## Usage
+## Simple Example Usage
 
 Start Server
 
@@ -27,3 +27,37 @@ Listening on port 3080
 Request => (Response Code: 301, Delay: 0 seconds)
 ```
 
+## Documentation
+
+* **Method GET**: Get endpoints responds with HTTP Status Code and empty content. 
+* **Method POST**: Post endpoints responds with HTTP Status Code and data sent by the client as part of request. 
+
+Detailed individual endpoint information is given bellow.
+
+--
+
+### GET `/random`
+
+* Gets random HTTP Status Code
+* Usage: `http://localhost/api/random/{delay}`
+* Method: GET
+
+
+##### URL Params
+
+Name          | Type          | Description
+------------- | ------------- | -----------------------------
+delay         | integer       | (Optional) Delay in seconds before response is sent.
+
+##### Example 1:
+
+* Request: `/random/5`
+* Code: 200 OK
+* Content: 
+
+##### Example 2: 
+* Request: `/random`
+* Code: 401 UNAUTHORIZED 
+* Content:
+
+**Notes**	Delay value must to be between 1 and 600 else value is ignored and response is returned immediately. 
--- index.js
+++ index.js
@@ -3,20 +3,16 @@ var express = require('express');
 
 const SERVER_PORT = 3080;
 const UNAVAILABLE_CODES = [100, 101];
-const VALID_CODES = [ 200, 201, 202, 203, 204, 205, 206, 300, 301, 302, 303, 304, 305, 306, 307, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 500, 501, 502, 503, 504, 505];
+const VALID_CODES = [200, 201, 202, 203, 204, 205, 206, 300, 301, 302, 303, 304, 305, 306, 307, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 500, 501, 502, 503, 504, 505];
 
-function apiHandler(req, res) {
-  console.log('Request => (Response Code: ' + req.params.code + ', Delay: ' + req.params.delay + ' seconds)');
-
-  var code = parseInt(req.params.code, 10);
-  if (UNAVAILABLE_CODES.indexOf(code) > -1){
+function apiSend(code, delay, req, res) {
+  if (UNAVAILABLE_CODES.indexOf(code) > -1) {
     return res.send('Sorry, that\'s a valid code, but this server cannot respond with ' + code);
   }
   if (VALID_CODES.indexOf(code) < 0) {
     return res.send('Invalid Code: ' + code);
   }
 
-  var delay = parseInt(req.params.delay, 10);
   if (delay < 0 || delay > 120) {
     return res.send('Invalid Delay: ' + delay + ', must be between 0 and 120 seconds');
   }
@@ -30,9 +26,26 @@ function apiHandler(req, res) {
   }, delay * 1000);
 }
 
+function apiGetHandler(req, res) {
+  var code = parseInt(req.params.code, 10);
+  var delay = req.params.delay ? parseInt(req.params.delay, 10) : 0;
+  console.log('Get => (Response Code: ' + code + ', Delay: ' + delay + ' seconds)');
+  apiSend(code, delay, req, res);
+}
+
+function apiRandomHandler(req, res) {
+  var code = VALID_CODES[Math.round(Math.random() * VALID_CODES.length)];
+  var delay = req.params.delay ? parseInt(req.params.delay, 10) : 0;
+  console.log('Random => (Response Code: ' + code + ', Delay: ' + delay + ' seconds)');
+  apiSend(code, delay, req, res);
+}
+
 var app = express();
 
-app.get('/api/:code(\\d{3})/:delay(\\d*)', apiHandler);
+app.get('/api/random', apiRandomHandler);
+app.get('/api/random/:delay(\\d*)', apiRandomHandler);
+app.get('/api/:code(\\d{3})', apiGetHandler);
+app.get('/api/:code(\\d{3})/:delay(\\d*)', apiGetHandler);
 
 app.listen(SERVER_PORT, function () {
   console.log('Listening on port ' + SERVER_PORT);
